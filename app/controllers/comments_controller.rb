bae8b569940ca080cf0c1195e550e7a5cfd63d39
gilbert_marquez <gilbert.marquez@ucr.ac.cr>
2024-04-15 02:55:47 -0600

    Implementación del controlador de comments: se hace el POST correctamente y se persisten los comentarios en la base de datos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+    # To deactivate CSRF protection to send requests
+    skip_before_action :verify_authenticity_token
+    before_action :set_feature
+
+    # Creates a comment, saves it and shows message
+    def create
+        @comment = @feature.comments.build(comment_params)
+        puts "CREATED"
+        if @comment.save
+            render json: @comment, status: :created
+        else
+            render json: @comment.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    
+    # Set feature associated to the comment 
+    def set_feature
+        @feature = Feature.find(params[:feature_id])
+    end
+    # Specify parameter
+    def comment_params
+        params.permit(:body)
+    end
 end
