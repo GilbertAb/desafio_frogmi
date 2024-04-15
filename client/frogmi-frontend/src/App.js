6942b0d4521b6fb25d225d6e350ad4381b0ce3d8
gilbert_marquez <gilbert.marquez@ucr.ac.cr>
2024-04-15 17:45:04 -0600

    Se implementó filtrado por mag_type

--- client/frogmi-frontend/src/App.js
+++ client/frogmi-frontend/src/App.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import FeatureCard from './components/FeatureCard';
-import FilterFeatures from './components/FilterFeatures';
 
 const App = () => {
   const [features, setFeatures] = useState([]);
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const [featuresPerPage, setFeaturesPerPage] = useState(10);
+  // Filter by mag_type
+  const [filterValue, setFilterValue] = useState('');
 
   // Obtain Features when loading the page
   useEffect(() => {
-    axios.get(`http://localhost:3000/features/index?page=${currentPage}&per_page=${featuresPerPage}`)
+    axios.get(`http://localhost:3000/features/index?page=${currentPage}&per_page=${featuresPerPage}&mag_type=${filterValue}`)
       .then(response => {
         setFeatures(response.data.data);
       })
       .catch(error => {
         console.error('Error fetching features:', error);
       });
-  }, [currentPage, featuresPerPage]);
+  }, [currentPage, featuresPerPage, filterValue]);
   
   const goToPreviousPage = () => {
     if (currentPage > 1) {
@@ -39,12 +40,28 @@ const App = () => {
     setCurrentPage(1);
   };
 
-
+  const handleFilterChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    setFilterValue(value);
+  };
 
   return (
     <div style={{paddingLeft:20}}>
+      {/* Title */}
       <h1>Features USGS</h1>
-
+      
+      {/* Filter by magnitude type */}
+      <div style={{marginLeft:40, marginTop:20}}>
+        Filter by magnitude type
+        <input style={{margin:10}}
+          type="text" 
+          placeholder={`Filter by magnitude type`} 
+          value={filterValue}
+          onChange={handleFilterChange} 
+        />
+      </div>
+      
+      {/* List of features */}
       <div style={{paddingLeft:20}}>
         <ul>
           {features.map(feature => (
@@ -52,15 +69,17 @@ const App = () => {
           ))}
         </ul>
       </div>
-      
-      <div>
-        <button onClick={goToPreviousPage} disabled={currentPage === 1}>Previous Page</button>
-        <button onClick={goToNextPage}>Next Page</button>
-        <span>Features per Page:</span>
+
+      {/* Pagination */}
+      <div style={{margin:30}}>
+        <button onClick={goToPreviousPage} disabled={currentPage === 1} style={{margin:10}}>Previous Page</button>
+        <button onClick={goToNextPage} style={{marginRight:10}}>Next Page</button>
+        <span style={{marginLeft:15, marginRight:5}}>Features per Page:</span>
         <input type="text" value={featuresPerPage} onChange={handleFeaturesPerPageChange}>
           
         </input>
       </div>
+      
 
     </div>
   );
