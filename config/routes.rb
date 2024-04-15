6e6558cb2c84e8e121d95a5c85807d2b1aaf3790
gilbert_marquez <gilbert.marquez@ucr.ac.cr>
2024-04-15 00:14:16 -0600

    Se crea el controlador (los archivos base) y se modifica la ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'features/index'
+  
+  # Route for POST endpoint that creates a comment associated to a feature
+  resources :features do
+    resources :comments, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
